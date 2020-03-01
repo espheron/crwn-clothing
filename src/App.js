cb17b1faf54a328fbaca4f9ecf4ca6c96e1bfbb0
Espheron <wondergemamber@gmail.com>
2020-03-01 16:08:00 +0100

    updated with shop page, collection preview & items

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
+ <div>
+   <h1>HATS PAGE</h1>
+ </div>
 );
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
         <Route path='/shop/hats' component={HatsPage} />
       </Switch>
     </div>
